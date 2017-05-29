9199e346b9cc5636273e6ff22c133b6fd403aae5
Edwin Delgado <edwindelgado@Edwins-MacBook-Pro.local>
2017-05-29 13:22:54 +0200

    video sixth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,31 +5,34 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Curso jQuery: Funciones</title>
+        <title>Curso jQuery: Arrays</title>
 
     </head>
     <body>
         <script type="text/javascript">
 
-            function factorial_1(num)
-            {
-                var fac =1;
-                for (num;num>0;num--)
-                    fac = fac * num;
-                return fac;
-            }
+            var arreglo = new Array();
 
-            var resultado = factorial_1(3);
-            alert(resultado);
+            arreglo.push(3);
+            arreglo.push('hola mundo');
+            arreglo[0] = 'Edwin';
+            console.log(arreglo[0]);
 
-            var factorial_2 = function (num){
-                var fac = 1;
-                for(num;num>0;num--)
-                    fac = fac * num;
-                alert(fac)
-            }
+            var arreglo1 = [1,2,'tres',4 ];
+            console.log(arreglo1.length);
 
-            factorial_2(5);
+            var arreglo2 = [[1,2,3,],['a','b','c','d','e']];
+            console.log(arreglo2[0].length);
+
+            var cadena = arreglo2[1].join('');
+            console.log(cadena);
+            var arreglo3 = cadena.split('');
+            console.log(arreglo3);
+
+            var cadena1 = 'a-bc';
+            var arreglo4 = cadena1.split('-');
+            console.log(arreglo4[0]);
+            console.log(arreglo4[1]);
 
         </script>
     </body>
