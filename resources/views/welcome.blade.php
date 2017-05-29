96feb78d5be0c5943f4d0b3eb17eccf126da2111
Edwin Delgado <edwindelgado@Edwins-MacBook-Pro.local>
2017-05-29 11:52:20 +0200

    second video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,45 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>jQuery</title>
+        <title>Curso jQuery: Condicionales</title>
 
     </head>
     <body>
         <script type="text/javascript">
-            var name = 'Codigo Facilito';
-            var number = 5;
-            alert('hola ' +  name + 'numero: ' + number);
+            
+            
+            var edad = 17;
+            if(edad >18)
+                alert('Eres mayor de 18 anos');
+            else if(edad<18)
+                alert('Eres menor de edad');
+            else
+                alert('Tienes 18 anos')
+            
+
+            
+            var aux = false;
+
+            var asignada = aux?'hola':'adios';
+
+            alert(asignada);
+            
+
+            var a =3;
+
+            switch(a)
+            {
+                case 0:
+                    alert('a = 0')
+                    break;
+                case 1:
+                    alert('a = 1')
+                    break;
+                default:
+                    alert('a = ?')
+                break;
+            }
+
         </script>
     </body>
 </html>
