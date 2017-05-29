e75240f56fc7453597eb7b7ad9d04f8fff920f3a
Edwin Delgado <edwindelgado@Edwins-MacBook-Pro.local>
2017-05-29 12:43:10 +0200

    third video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,44 +5,31 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Curso jQuery: Condicionales</title>
+        <title>Curso jQuery: Funciones</title>
 
     </head>
     <body>
         <script type="text/javascript">
-            
-            
-            var edad = 17;
-            if(edad >18)
-                alert('Eres mayor de 18 anos');
-            else if(edad<18)
-                alert('Eres menor de edad');
-            else
-                alert('Tienes 18 anos')
-            
 
-            
-            var aux = false;
-
-            var asignada = aux?'hola':'adios';
-
-            alert(asignada);
-            
-
-            var a =3;
-
-            switch(a)
+            function factorial_1(num)
             {
-                case 0:
-                    alert('a = 0')
-                    break;
-                case 1:
-                    alert('a = 1')
-                    break;
-                default:
-                    alert('a = ?')
-                break;
+                var fac =1;
+                for (num;num>0;num--)
+                    fac = fac * num;
+                return fac;
             }
+
+            var resultado = factorial_1(3);
+            alert(resultado);
+
+            var factorial_2 = function (num){
+                var fac = 1;
+                for(num;num>0;num--)
+                    fac = fac * num;
+                alert(fac)
+            }
+
+            factorial_2(5);
 
         </script>
     </body>
